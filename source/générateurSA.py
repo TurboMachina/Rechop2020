e3f602e8c3f80d2614beb18393f1ac8303fb4123
AlexHerlinvaux <47380532+AlexHerlinvaux@users.noreply.github.com>
2020-12-15 15:29:24 +0100

    Je suis un gros retard mais ça marche

--- source/générateurSA.py
+++ source/générateurSA.py
@@ -6,130 +6,196 @@
 # -> Prendre un paramètre indice supplémentaire
 
 
-from source.classeSaut import Saut
-import scipy.stats as sc
-
-def hull_dobbel_periode(x0, a, c, m):
-    bigger = m if c < m else c
-    half_of_bigger = bigger // 2
-    for i in range(half_of_bigger + 1):
-        if(c % i == 0 and m % i == 0):
-            return False
-
-    for p in range(2, m//2):
-        if(est_premier(p) and m % p == 0):
-            if p % (a * (a - 1)) != 0: # verif si cette formule est bonne
-                return False
-
-    if(m % 4 == 0):
-        return ((a - 1) % 4) == 0 # on renvoie bien true ou false dans les bons cas ?
-        # on renvoie true ou false si le if passe, rien sinon ?
+from classeSaut import Saut
+from generateurSAiterable import GenerateurSA
+from xlwt import Workbook 
+from scipy import stats
+from functools import reduce
+import math
+import xlwt
 
 
-def generer_suite_aleatoire(x0, a, c ,m):
-    suite_aleatoire = []
-    suite_aleatoire.append(x0)
-    xn_plusun = (a * x0 + c) % m
-    while(xn_plusun != x0):
-        suite_aleatoire.append(xn_plusun)
-        xn = xn_plusun
-        xn_plusun = (a * xn + c) % m
-    return suite_aleatoire
 
-def est_premier(nombre):
-    half_of_number = nombre // 2
-    for i in range(2,half_of_number+1):
-        if(nombre % i == 0):
+#----------------------------------------------------------------------------
+#           génération suite
+#----------------------------------------------------------------------------
+
+#test si un nombre est premier
+def est_premier_entreeux(c,m):
+    for i in range(2,min(c,m)) :
+        if c%i == 0 and m%i == 0 :
             return False
     return True
 
-def merge(merge_start_id, merge_end_id, test_des_sauts):
-    new_saut = "[" + str(merge_start_id) + "-" + str(merge_end_id) + "]" # franchement, propre ça ? jsp
-    new_ri = 0
-    new_pi = 0
-    new_npi = 0
-    
-    for i in range(merge_end_id, merge_start_id+1):
-        new_pi = test_des_sauts[i].pi
-        new_ri = test_des_sauts[i].ri
-        new_npi = test_des_sauts[i].npi
-        test_des_sauts.pop(i)
-    
-    test_des_sauts[merge_end_id].saut = new_saut
-    test_des_sauts[merge_end_id].pi = new_pi
-    test_des_sauts[merge_end_id].ri = new_ri
-    test_des_sauts[merge_end_id].npi = new_npi
+estPremier = lambda x : not any ([x % i == 0 for i in range(2,x)])
 
-    replaceI = merge_end_id + 1
-    i = merge_start_id + 1
-    while(i < len(test_des_sauts)): # de base y a un != de null, mais useless ici en python j'imagine
-        test_des_sauts[replaceI] = test_des_sauts[i]
-        i += 1
-        replaceI += 1
 
-        return test_des_sauts
+#2 hypothese à vérifier pour Hull Dobell
+def fact_premier_m(m,a):
+    for p in range(2,int(m/2)):
+        if estPremier(p) and m%p == 0 :
+            if(p%(a*(a-1)) == 0):
+                return False
+    return True
 
-def test_des_sauts(suite_aleatoire, valeur_test_saut):
-    niveau_incertitude = 0.05
-    n = len(suite_aleatoire)
-    test_des_sauts = []
-    for i in range(n-2):
-        new_saut = Saut(i, (0.9**i)*0.1, None, n*(0.9**i)*0.1) # wtf le pi ici ?
-        test_des_sauts.append(new_saut)
+#3 hypothese à vérifier pour Hull Dobell          
+def m_mult_4(m,a):
+    if m % 4 == 0:
+        return (a-1) % 4 == 0
+    return True
+
+#test pour vérifier que c'est une suite de nombre aléatoire
+def Hull_Dobell(x0, a, c, m):
+    return est_premier_entreeux(c,m) and fact_premier_m(m,a) and m_mult_4(m, a)
+        
+#renvoie la suite aléatoire et en donne sa période
+def periode_serie(x0,a,c,m):
+    serie = [x0]
+    x1 = (a*x0 + c) %m
+    serie.append(x1)  
+    for iSerie in range(2,m):
+        xn = (a*serie[iSerie-1] + c) %m
+        if xn == x1:
+            return (iSerie-1, serie)
+        serie.append(xn)
+    return (m, serie)
+
+#----------------------------------------------------------------------------
+#           Tests validités
+#----------------------------------------------------------------------------
+
+
+def test_des_sauts(suite_aleatoire, valeur_test_saut, m):
+    n = 0
+    wb = Workbook()
+    preRegroup = wb.add_sheet("Pre-regroupement")
+    preRegroup.write(0,0, 'Longueur du saut')
+    preRegroup.write(0,1, 'Ri')
+    preRegroup.write(0,2, 'Pi')
+    preRegroup.write(0,3, 'N*Pi')
+    postRegroup = wb.add_sheet("Post-regroupement")
+    postRegroup.write(0,0, 'Longueur du saut')
+    postRegroup.write(0,1, 'Ri')
+    postRegroup.write(0,2, 'Pi')
+    postRegroup.write(0,3, 'N*Pi')
+    # le test s'intéresse aux longueurs des sauts avant de recroiser une valeur
+    # de la suite
     
-    index_val = 0
-    while(suite_aleatoire[index_val] != valeur_test_saut and index_val < n):
-        index_val += 1
+    # etape 1
+    #H0 : la suite xn est acceptable
+    #H1 : la suite xn n'est pas acceptable
+    
+    #etape 2
+    alpha = 0.05
+    
+    #etape 3
+    #créations des longueurs de sauts
+    tabSauts = []
+    for i in range (0,m-2):
+        pi = (0.9**i)*0.1
+        tabSauts.append(Saut(i,pi,0,0))
+        
+    #parcours de la suite pour incrémenter les ri correspondant
+    x = math.floor((suite_aleatoire._x0/m)*10)
+    while(x != valeur_test_saut) :
+        x = math.floor((next(suite_aleatoire)/m)*10)
     saut = 0
-    for i in range(index_val+1, n):
-        if(suite_aleatoire[i] != valeur_test_saut):
-            saut += 1
+    while(x != -1) :
+        x = math.floor((next(suite_aleatoire)/m)*10)
+        if(x != valeur_test_saut):
+            saut+=1
         else:
-            test_des_sauts[saut].ri += 1
+            tabSauts[saut]._ri += 1
+            n+= 1
             saut = 0
-
-    for i in range(n-1,-1,-1):
-        if(test_des_sauts[i].npi < 5):
-            npi_temp = test_des_sauts[i].npi
-            merge_start_id = i
-            while(i >= 0 and npi_temp > 5):
-                i -= 1
-                npi_temp += test_des_sauts[i].npi
-            merge_end_id = i
-            test_des_sauts = merge(merge_start_id, merge_end_id, test_des_sauts)
+            
+    #determine n
+    #ecriture des résultat pré-regroupement
+    for i in range(0, len(tabSauts)):
+        tabSauts[i]._npi = n * tabSauts[i].pi
+        preRegroup.write(i+1,0, tabSauts[i].saut)
+        preRegroup.write(i+1,1, tabSauts[i].ri)
+        preRegroup.write(i+1,2, tabSauts[i].pi)
+        preRegroup.write(i+1,3, tabSauts[i].npi)
     
-    # Methode 1
-    khi_carre = 0
-    for i in range(len(test_des_sauts)):
-        tds = test_des_sauts[i]
-        khi_carre += ((tds.ri - tds.pi)**2) / (tds.npi)
-    degre_de_liberte = len(test_des_sauts) - 1
-    proba = 1 - niveau_incertitude
-    valeur_max = 0 # lecture de table ici
-
-    return khi_carre >= 0 and khi_carre <= valeur_max
-
-    # Methode 2
-    stat, p, dof, expected = sc.chi2_contingency(test_des_sauts)
-    crit = sc.chi2.ppf(proba, dof) # go creuser la doc de scipy pour voir comment ça marche
-    if abs(stat) >= crit:
-	    return False
-    else:
-	    return True
-
-def main_generateur():
-    x0 = input()
-    a  = input()
-    c = input()
-    m = input()
-    valeurTestSaut = input()
-
-    if(not hull_dobbel_periode(x0, a, c, m)):
+    #etape 4
+    #regroupement pour que n*pi > 5
+    nb_classes_regroup = 0
+    for i in range(len(tabSauts)-1,0,-1):
+        if tabSauts[i].npi < 5 :
+            nb_classes_regroup += 1
+            tabSauts[i-1]._npi += tabSauts[i].npi
+            tabSauts[i-1]._ri += tabSauts[i].ri
+            tabSauts[i-1]._pi += tabSauts[i].pi
+            if(type(tabSauts[i].saut) == int):
+                tabSauts[i-1]._saut = (tabSauts[i-1].saut,tabSauts[i].saut)
+            else:
+                pre,post = tabSauts[i].saut
+                tabSauts[i-1]._saut = (tabSauts[i-1].saut,post)
+            del tabSauts[i]
+    
+    #ecriture des résultat pré-regroupement
+    for i in range(0, len(tabSauts)):
+        if(type(tabSauts[i].saut) == int):
+            postRegroup.write(i+1,0, tabSauts[i].saut)
+        else :
+            pre, post = tabSauts[i].saut
+            postRegroup.write(i+1,0, str(pre) + " - " + str(post) )
+            
+        postRegroup.write(i+1,1, tabSauts[i].ri)
+        postRegroup.write(i+1,2, tabSauts[i].pi)
+        postRegroup.write(i+1,3, tabSauts[i].npi)
+    
+    last_line = i    
+    
+    
+    
+    #etape 5
+    #calcul zone de rejet à partir de la table de Khi carré théorique
+    deb_reg_rejet = stats.chi2.ppf(1 - alpha, len(tabSauts) - 1)
+    #calcul du Khi carré observé
+    X_obs = []
+    for i in range(0, len(tabSauts)):
+        X_obs.append(math.pow(tabSauts[i].ri- tabSauts[i].npi,2)/ tabSauts[i].npi)
+    
+    X_obs = reduce(lambda x,y : x+y, X_obs)
+    postRegroup.write(last_line+3, 0, "Khi² observé :")
+    postRegroup.write(last_line+3, 1, X_obs)
+    postRegroup.write(last_line+4, 0, "Khi² théorique :")
+    postRegroup.write(last_line+4, 1, deb_reg_rejet)
+    
+    #etape 6
+    if deb_reg_rejet < X_obs :
+        postRegroup.write(last_line+5, 0, "rejet hypothese nulle, la suite n'est pas acceptable au test des courses")
+        return False
+    else :
+        postRegroup.write(last_line+5, 0, "non rejet hypothese nulle, la suite est acceptable au test des courses")
+        return True
+    
+    wb.save("adm_" + str(valeur_test_saut) + ".xls")
+    
+def main():
+    x0 = 5
+    a  = 61
+    c = 7
+    m = 600
+    tabTest = []
+    if(not Hull_Dobell(x0, a, c, m)):
         print("Paramètres incorrects")
         return None
-    suite_aleatoire = generer_suite_aleatoire(x0, a, c, m)
-    if(test_des_sauts(suite_aleatoire, valeurTestSaut)):
-        print("Test des sauts foiré XD")
-        return None
-
-    return x0, a, c, m, suite_aleatoire # pas dans le DA mais utile pour après
+    else:
+        print("Paramètres validés par Théoréme de Hull-Dobell\n=========\n=========\n=========")
+        suite_aleatoire = GenerateurSA(x0, a, c, m)
+        for valeur_test_saut in range(0,9):
+            suite_aleatoire.reset()
+            tabTest.append(True) if (test_des_sauts(suite_aleatoire, valeur_test_saut, m)) else tabTest.append(False)
+            print("Le test des sauts est réussi pour la valeur " +  str(valeur_test_saut)) if all(tabTest) else print("Le test des sauts à échouer pour la valeur " + str(valeur_test_saut))
+        if all(tabTest):
+            print("=========\n=========\n=========\nLe test des sauts est réussi pour toutes les valeurs")
+        else :
+            tabFalse= []
+            for i in range(0,len(tabTest)):
+                if not tabTest[i] :
+                    tabFalse.append(i)
+            print("Le test des sauts à échoue pour les valeurs : " + str(tabFalse))
+    return x0, a, c, m
